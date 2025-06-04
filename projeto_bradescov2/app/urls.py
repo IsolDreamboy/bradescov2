abe49497a84e778113751a3adfe209b1e76cf12a
IsolDreamboy <edicarlosmfilho@gmail.com>
2025-06-04 19:16:22 -0300

    Delete projeto_bradescov2 directory

--- projeto_bradescov2/app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import RegistroView, LoginView, ValoresReceberView
-
-urlpatterns = [
-    path('registro/', RegistroView.as_view()),
-    path('login/', LoginView.as_view()),
-    path('valores-a-receber/', ValoresReceberView.as_view()),
-]
